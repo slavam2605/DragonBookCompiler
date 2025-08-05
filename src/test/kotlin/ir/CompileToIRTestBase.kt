bcc281e54eea5584d565f260381ad5aa58e90ed0
Vyacheslav Moklev <vyacheslav.moklev@jetbrains.com>
2025-08-05 13:43:32 +0300

    Implement negative scope tests

--- src/test/kotlin/ir/CompileToIRTestBase.kt
+++ src/test/kotlin/ir/CompileToIRTestBase.kt
@@ -2,7 +2,9 @@ package ir
 
 import MainGrammar
 import MainLexer
+import compiler.frontend.CompilationFailed
 import compiler.frontend.CompileToIRVisitor
+import compiler.frontend.SemanticAnalysisVisitor
 import compiler.ir.IRProtoNode
 import compiler.ir.IRVar
 import compiler.ir.cfg.ControlFlowGraph
@@ -11,13 +13,14 @@ import ir.interpreter.CFGInterpreter
 import ir.interpreter.ProtoIRInterpreter
 import org.antlr.v4.runtime.CharStreams
 import org.antlr.v4.runtime.CommonTokenStream
-import org.example.parser.UnderlineErrorListener
+import parser.UnderlineErrorListener
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.DynamicContainer
 import org.junit.jupiter.api.DynamicNode
 import org.junit.jupiter.api.DynamicTest
 import java.io.File
 import kotlin.random.Random
+import kotlin.test.assertTrue
 
 abstract class CompileToIRTestBase {
     protected enum class TestMode {
@@ -31,6 +34,7 @@ abstract class CompileToIRTestBase {
             addErrorListener(UnderlineErrorListener())
         }
         val tree = parser.program()
+        SemanticAnalysisVisitor().analyze(tree)
         return CompileToIRVisitor().compileToIR(tree)
     }
 
@@ -70,19 +74,45 @@ abstract class CompileToIRTestBase {
         return DynamicTest.dynamicTest(file.name) {
             val testProgram = file.readText()
             val expectedRegex = "// *expected: *([a-zA-Z0-9_]+) *== *([0-9]+)".toRegex()
-            val assertions = testProgram.lines().mapNotNull { line ->
+            val expectedValues = testProgram.lines().mapNotNull { line ->
                 expectedRegex.find(line)?.let {
                     val (varName, expectedValue) = it.destructured
-                    varName to expectedValue.toLong()
+                    ExpectedValue(varName, expectedValue.toLong())
                 }
             }
-            check(assertions.isNotEmpty()) { "No assertions found in file $file" }
-            val result = compileAndRun(mode, testProgram)
-            assertions.forEach { (varName, expectedValue) ->
-                val actualValue = result.getVariable(varName)
-                assertEquals(expectedValue, actualValue) {
-                    "Expected $varName == $expectedValue, but got $actualValue"
+            val errorRegex = "// *error: *([0-9]+):([0-9]+)* *\"(.*)\"".toRegex()
+            val expectedErrors = testProgram.lines().mapNotNull { line ->
+                errorRegex.find(line)?.let {
+                    val (line, col, message) = it.destructured
+                    ExpectedError(line.toInt(), col.toInt(), message)
                 }
+            }
+            check(expectedValues.isNotEmpty() || expectedErrors.isNotEmpty()) {
+                "No assertions found in file $file"
+            }
+
+            val visitedErrors = mutableSetOf<ExpectedError>()
+            try {
+                val result = compileAndRun(mode, testProgram)
+                expectedValues.forEach { (varName, expectedValue) ->
+                    val actualValue = result.getVariable(varName)
+                    assertEquals(expectedValue, actualValue) {
+                        "Expected $varName == $expectedValue, but got $actualValue"
+                    }
+                }
+            } catch (e: CompilationFailed) {
+                e.exceptions.forEach { exception ->
+                    val ctx = exception.ctx
+                    val expectedError = expectedErrors.find {
+                        it.line == ctx.line && it.col == ctx.start && it.message == exception.message
+                    }
+                    assertTrue(expectedError != null, "Unexpected error: ${ctx.line}:${ctx.start} \"${exception.message}\"")
+                    visitedErrors.add(expectedError)
+                    println("Expected exception: ${exception.message} at ${ctx.line}:${ctx.start}")
+                }
+            }
+            expectedErrors.forEach { error ->
+                assertTrue(error in visitedErrors, "Expected error was not thrown: ${error.line}:${error.col} \"${error.message}\"")
             }
         }
     }
@@ -122,4 +152,8 @@ abstract class CompileToIRTestBase {
             DynamicContainer.dynamicContainer("$mode", nodes)
         }
     }
+
+    private data class ExpectedValue(val varName: String, val expectedValue: Long)
+
+    private data class ExpectedError(val line: Int, val col: Int, val message: String)
 }

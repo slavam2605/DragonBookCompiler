86457119e1b0afed8f83513c9aedfee405b3f52e
Vyacheslav Moklev <vyacheslav.moklev@jetbrains.com>
2025-08-07 21:33:45 +0300

    Optimize SSA transformation

---
+++ src/main/kotlin/compiler/ir/cfg/ssa/RenameVariablesForSSA.kt
@@ -0,0 +1,95 @@
+package compiler.ir.cfg.ssa
+
+import compiler.ir.BaseIRTransformer
+import compiler.ir.IRLabel
+import compiler.ir.IRNode
+import compiler.ir.IRPhi
+import compiler.ir.IRValue
+import compiler.ir.IRVar
+import compiler.ir.cfg.ControlFlowGraph
+import compiler.ir.cfg.DominatorTree
+import kotlin.collections.forEach
+import kotlin.collections.forEachIndexed
+import kotlin.collections.get
+import kotlin.collections.last
+
+private class BlockRecord(val label: IRLabel, var index: Int)
+
+internal class RenameVariablesForSSA(
+    private val cfg: ControlFlowGraph,
+    private val mutableBlocks: MutableMap<IRLabel, MutableList<IRNode>>
+) {
+    private val counter = mutableMapOf<String, Int>()
+    private val stack = mutableMapOf<String, MutableList<BlockRecord>>()
+    private val dominatorTree = DominatorTree.get(cfg)
+
+    internal fun renameVariables() = renameBlock(cfg.root)
+
+    private fun peekName(x: String): Int {
+        val xStack = stack[x] ?: return 0
+        return xStack.last().index
+    }
+
+    private fun newName(label: IRLabel, x: IRVar): IRVar {
+        val index = counter.compute(x.name) { _, i -> (i ?: 0) + 1 }!!
+        val xStack = stack.getOrPut(x.name) { mutableListOf() }
+        if (xStack.isEmpty() || xStack.last().label != label) {
+            xStack.add(BlockRecord(label, index))
+        } else {
+            xStack.last().index = index
+        }
+        return IRVar(x.name, index)
+    }
+
+    private fun renameBlock(label: IRLabel) {
+        // Step 1. Rename all variables in the block
+        val block = mutableBlocks[label]!!
+        var isInPhiPrefix = true
+        block.forEachIndexed { index, node ->
+            if (node !is IRPhi) isInPhiPrefix = false
+            check(isInPhiPrefix || node !is IRPhi)
+
+            if (node is IRPhi) {
+                block[index] = IRPhi(newName(label, node.result), node.sources)
+            } else {
+                val renamedVars = node.rvalues()
+                    .filterIsInstance<IRVar>()
+                    .associateWith {
+                        val ssaIndex = peekName(it.name)
+                        IRVar(it.name, ssaIndex)
+                    }
+                val lVars = node.lvalues()
+                check(lVars.size <= 2 || lVars.distinct().size == lVars.size)
+                val newLVars = lVars.associateWith { newName(label, it) }
+                block[index] = node.transform(object : BaseIRTransformer() {
+                    override fun transformLValue(value: IRVar) = newLVars[value]!!
+                    override fun transformRValue(value: IRValue) = renamedVars[value] ?: value
+                })
+            }
+        }
+
+        // Step 2. Fill in phi-nodes in **successor blocks of CFG**
+        cfg.edges(label).forEach { successor ->
+            val nextBlock = mutableBlocks[successor]!!
+            val selfIndex = cfg.getBlockIndex(label, successor)
+            nextBlock.forEachIndexed { index, node ->
+                if (node !is IRPhi) return@forEachIndexed
+                val sourceVar = node.sources[selfIndex]
+                val lastSSAVer = peekName(sourceVar.name)
+                nextBlock[index] = node.replaceSourceAt(selfIndex, IRVar(sourceVar.name, lastSSAVer))
+            }
+        }
+
+        // Step 3. Recursively handle all **successor blocks in the dominator tree**
+        dominatorTree.edges(label).forEach { successor ->
+            renameBlock(successor)
+        }
+
+        // Step 4. Unwind stacks modified in this block
+        stack.forEach { (_, stack) ->
+            if (stack.lastOrNull()?.label == label) {
+                stack.removeLast()
+            }
+        }
+    }
+}
---
+++ src/main/kotlin/compiler/ir/cfg/ssa/SSAControlFlowGraph.kt
@@ -0,0 +1,91 @@
+package compiler.ir.cfg.ssa
+
+import compiler.ir.IRLabel
+import compiler.ir.IRNode
+import compiler.ir.IRPhi
+import compiler.ir.IRVar
+import compiler.ir.cfg.CFGBlock
+import compiler.ir.cfg.ControlFlowGraph
+import compiler.ir.cfg.DominanceFrontiers
+
+/**
+ * Special marker implementation of [compiler.ir.cfg.ControlFlowGraph] that guarantees that it is in SSA form.
+ */
+class SSAControlFlowGraph(root: IRLabel, blocks: Map<IRLabel, CFGBlock>) : ControlFlowGraph(root, blocks) {
+    companion object {
+        /**
+         * Transforms the given [ControlFlowGraph] into an [SSAControlFlowGraph].
+         */
+        fun transform(cfg: ControlFlowGraph): SSAControlFlowGraph {
+            val mutableBlocks = cfg.blocks.mapValues { (_, block) ->
+                block.irNodes.toMutableList()
+            }.toMutableMap()
+
+            // Step 1. Compute "global" variables that are used in more than one block
+            val (globals, blocksMap) = getGlobals(cfg)
+
+            // Step 2. Insert phi nodes for the "global" variables using dominance frontiers
+            insertPhiNodes(cfg, blocksMap, globals, mutableBlocks)
+
+            // Step 3. Rename all variables to have unique names
+            RenameVariablesForSSA(cfg, mutableBlocks).renameVariables()
+
+            return SSAControlFlowGraph(
+                cfg.root,
+                mutableBlocks.mapValues { (_, irNodes) ->
+                    CFGBlock(irNodes)
+                }
+            )
+        }
+
+        private fun insertPhiNodes(
+            cfg: ControlFlowGraph,
+            blocksMap: Map<IRVar, Set<IRLabel>>,
+            globals: Set<IRVar>,
+            mutableBlocks: MutableMap<IRLabel, MutableList<IRNode>>
+        ) {
+            val df = DominanceFrontiers.get(cfg)
+            globals.forEach { globalVar ->
+                // If `block in scheduled`, then `block` already has `x = phi(...)`
+                // and `block` is in the work list now, or was already processed
+                val scheduled = mutableSetOf<IRLabel>()
+
+                val workList = blocksMap[globalVar]!!.toMutableList()
+                while (workList.isNotEmpty()) {
+                    val currentLabel = workList.removeLast()
+                    df[currentLabel]!!.forEach { frontierLabel ->
+                        if (frontierLabel in scheduled) {
+                            return@forEach // `phi` already inserted, skip block
+                        }
+
+                        val predCount = cfg.backEdges(frontierLabel).size
+                        val phi = IRPhi(globalVar, List(predCount) { globalVar })
+                        mutableBlocks[frontierLabel]!!.add(0, phi)
+                        scheduled.add(frontierLabel)
+                        workList.add(frontierLabel)
+                    }
+                }
+            }
+        }
+
+        private fun getGlobals(cfg: ControlFlowGraph): Pair<Set<IRVar>, Map<IRVar, Set<IRLabel>>> {
+            val globals = mutableSetOf<IRVar>()
+            val blocksMap = mutableMapOf<IRVar, MutableSet<IRLabel>>()
+            cfg.blocks.forEach { (label, block) ->
+                val varKill = mutableSetOf<IRVar>()
+                block.irNodes.forEach { irNode ->
+                    irNode.rvalues().filterIsInstance<IRVar>().forEach { rVar ->
+                        if (rVar !in varKill) {
+                            globals.add(rVar)
+                        }
+                    }
+                    irNode.lvalues().forEach { lVar ->
+                        varKill.add(lVar)
+                        blocksMap.getOrPut(lVar) { mutableSetOf() }.add(label)
+                    }
+                }
+            }
+            return globals to blocksMap
+        }
+    }
+}

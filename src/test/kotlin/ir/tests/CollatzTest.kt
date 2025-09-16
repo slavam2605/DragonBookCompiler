d2120aac658e67dfa0c773de6a928e6f613da16f
Vyacheslav Moklev <vyacheslav.moklev@jetbrains.com>
2025-09-16 14:04:24 +0300

    Speed up collatz test

--- src/test/kotlin/ir/tests/CollatzTest.kt
+++ src/test/kotlin/ir/tests/CollatzTest.kt
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.DynamicTest
 import org.junit.jupiter.api.TestFactory
 
 class CollatzTest : CompileToIRTestBase() {
+    override val customNativeRunner = "collatz/native/runner.c"
+
     private fun collatz(n: Long) = generateSequence(n) {
         when {
             it == 1L -> null
@@ -19,13 +21,17 @@ class CollatzTest : CompileToIRTestBase() {
     fun testCollatz(): List<DynamicNode> {
         val parameters = generateRandomParameters(100, 1L ..Int.MAX_VALUE / 20).map { it * it }
         return withParametersAndFiles(parameters, "/collatz") { mode, n, file ->
-            DynamicTest.dynamicTest("${file.name} [n = $n]") {
+            if (mode == TestMode.NATIVE_ARM64 && n != parameters[0]) return@withParametersAndFiles null
+            val testName = if (mode == TestMode.NATIVE_ARM64) "[all n]" else "[n = $n]"
+            DynamicTest.dynamicTest(testName) {
                 val program = readWithPattern(file, "n" to n)
                 val result = compileAndGetResult(mode, program)
                 if (PRINT_DEBUG_INFO) {
                     println("collatz steps for $n = $result")
                 }
-                assertEquals(collatz(n).toLong(), result)
+                if (mode != TestMode.NATIVE_ARM64) {
+                    assertEquals(collatz(n).toLong(), result)
+                }
             }
         }
     }

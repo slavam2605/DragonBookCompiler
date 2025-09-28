8c61f6b0b8a32243ea55740f5003d3b03e456716
Vyacheslav Moklev <vyacheslav.moklev@jetbrains.com>
2025-09-28 13:48:32 +0300

    Fix end-of-line parsing in the case of multiline block comments

--- src/main/kotlin/parser/ParserUtils.kt
+++ src/main/kotlin/parser/ParserUtils.kt
@@ -10,17 +10,33 @@ object ParserUtils {
     @JvmStatic
     fun isEndOfStatement(input: TokenStream): Boolean {
         check(input is BufferedTokenStream)
-        val lineBreaks = input.getHiddenTokensToLeft(input.index(), MainLexer.LINE_BREAK)
-        if (lineBreaks?.isNotEmpty() == true) {
-            return true
-        }
+        if (hasLineBreak(input)) return true
+
+        // Check for obvious "end" tokens
         return input.LA(1) in endTokens
     }
 
     @JvmStatic
     fun noLineBreaks(input: TokenStream): Boolean {
         check(input is BufferedTokenStream)
+        return !hasLineBreak(input)
+    }
+
+    private fun hasLineBreak(input: BufferedTokenStream): Boolean {
+        // Try to find an explicit line break
         val lineBreaks = input.getHiddenTokensToLeft(input.index(), MainLexer.LINE_BREAK)
-        return lineBreaks?.isEmpty() ?: true
+        if (lineBreaks?.isNotEmpty() == true) {
+            return true
+        }
+
+        // Try to find a block comment with a line break inside
+        val comments = input.getHiddenTokensToLeft(input.index(), MainLexer.COMMENTS)
+        comments?.forEach { token ->
+            if (token.text.contains('\n')) {
+                return true
+            }
+        }
+
+        return false
     }
 }

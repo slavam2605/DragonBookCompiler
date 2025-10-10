ee49ab17dd95e3f6fa2debd899867896289394e8
Vyacheslav Moklev <vyacheslav.moklev@jetbrains.com>
2025-10-10 10:40:53 +0300

    Add non-powers of two to the division test

--- src/test/resources/performance_tests/division/runner.cpp
+++ src/test/resources/performance_tests/division/runner.cpp
@@ -3,6 +3,7 @@
 #include <random>
 
 extern "C" long long calculate_div_p2(long long n);
+extern "C" long long calculate_div(long long n);
 
 long long calculate_div_gold_p2(long long n) {
     long long a = n / 1 + n / 2 + n / 4 + n / 8 + n / 1024 + n / 131072 + n / 1048576;
@@ -12,20 +13,38 @@ long long calculate_div_gold_p2(long long n) {
     return a + b + 3 * (c + d) / 7;
 }
 
+long long calculate_div_gold(long long n) {
+    long long a = n / 3 + n / 5 + n / 7 + n / 11 + n / 13 + n / 97 + n / 1009 + n / 128712 + n / 2147483647LL + n / 8589935681LL + n / 489133282872437279LL;
+    long long b = n % 3 + n % 5 + n % 7 + n % 11 + n % 13 + n % 97 + n % 1009 + n % 128712 + n % 2147483647LL + n % 8589935681LL + n % 489133282872437279LL;
+    long long c = n / -3 + n / -5 + n / -7 + n / -11 + n / -13 + n / -97 + n / -1009 + n / -128712 + n / -2147483647LL + n / -8589935681LL + n / -489133282872437279LL;
+    long long d = n % -3 + n % -5 + n % -7 + n % -11 + n % -13 + n % -97 + n % -1009 + n % -128712 + n % -2147483647LL + n % -8589935681LL + n % -489133282872437279LL;
+    return a + b + 2 * (c + d) / 5;
+}
+
 extern "C" long long performance_gold(long long n) {
     long long result = 0;
     for (long long i = 0; i < n; i++) {
-        result += calculate_div_gold_p2(i * i);
+        result += calculate_div_gold_p2(i * i) + calculate_div_gold(i * i + 1);
     }
     return result;
 }
 
 class MyRunnerInit {
-    void test(long long value) {
+    void test_p2(long long value) {
         long long expected = calculate_div_p2(value);
         long long actual = calculate_div_gold_p2(value);
         if (actual != expected) {
-            std::cout << "Wrong value for " << value << ": " <<
+            std::cout << "Wrong value for calculate_div_p2(" << value << "): " <<
+                "expected: " << expected << " actual: " << actual << std::endl;
+            exit(1);
+        }
+    }
+
+    void test(long long value) {
+        long long expected = calculate_div(value);
+        long long actual = calculate_div_gold(value);
+        if (actual != expected) {
+            std::cout << "Wrong value for calculate_div(" << value << "): " <<
                 "expected: " << expected << " actual: " << actual << std::endl;
             exit(1);
         }
@@ -50,24 +69,29 @@ class MyRunnerInit {
 
     }
 
+    void test_both(long long value) {
+        test_p2(value);
+        test(value);
+    }
+
 public:
     MyRunnerInit() {
-        test(0); test(1); test(-1); test(2); test(-2);
-        test(INT64_MIN); test(INT64_MIN + 1); test(INT64_MAX); test(INT64_MAX - 1);
+        test_both(0); test_both(1); test_both(-1); test_both(2); test_both(-2);
+        test_both(INT64_MIN); test_both(INT64_MIN + 1); test_both(INT64_MAX); test_both(INT64_MAX - 1);
         for (int shift = 0; shift < 64; shift++) {
             long long pow2 = 1 << shift;
-            test(pow2); test(pow2 + 1); test(pow2 - 1);
-            test(-pow2); test(-pow2 + 1); test(-pow2 - 1);
+            test_both(pow2); test_both(pow2 + 1); test_both(pow2 - 1);
+            test_both(-pow2); test_both(-pow2 + 1); test_both(-pow2 - 1);
         }
         run_log_tests(1, INT64_MAX, 10000000);
         run_log_tests(INT64_MIN, -1, 10000000);
         for (int i = -10000000; i <= 10000000; i++) {
-            test(i);
+            test_both(i);
         }
         std::mt19937_64 rng(42);
         std::uniform_int_distribution<int64_t> dist(INT64_MIN, INT64_MAX);
         for (int i = 0; i < 10000000; i++) {
-            test(dist(rng));
+            test_both(dist(rng));
         }
     }
 } myRunnerInit;
